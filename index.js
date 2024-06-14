3238c087f9462683766e6fb0861f63588166f8d3
jlounsb1 <jefflounsbury@gmail.com>
2024-06-14 14:48:23 -0400

    copied the adventureFactory function and created variables, and tested by logging some information

--- index.js
+++ index.js
@@ -96,3 +96,24 @@ class companion extends Character {
     }
 }
 
+class AdventurerFactory {  
+    constructor (role) {
+      this.role = role;
+      this.adventurers = [];
+    }
+    generate (name) {
+      const newAdventurer = new Adventurer(name, this.role);
+      this.adventurers.push(newAdventurer);
+    }
+    findByIndex (index) {
+      return this.adventurers[index];
+    }
+    findByName (name) {
+      return this.adventurers.find((a) => a.name === name);
+    }
+  }
+  
+  const healers = new AdventurerFactory("Healer");
+  const robin = healers.generate("Robin");
+  console.log(healers)
+  console.log(healers.adventurers[0].inventory)
